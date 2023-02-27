427a79925be102d9978f729c0642099347f08d19
Viktor Ristic <vktr.r2@protonmail.com>
2023-02-26 20:16:15 -0500

    Compass: Declaring and Calling Functions

---
+++ hello.js
@@ -0,0 +1,7 @@
+
+//Introduce sayHello function - takes 'name' as an arguement/parameter
+const sayHello = function (name) {
+  console.log('Hello, ' + name);
+}
+//Call function with my own name
+sayHello('Vik');
