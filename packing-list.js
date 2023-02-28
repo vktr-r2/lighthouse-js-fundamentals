7b185887b2ed77020430c2b56e7c2282e6930742
Viktor Ristic <vktr.r2@protonmail.com>
2023-02-28 00:31:38 -0500

    Compass-Packing List assignment

---
+++ packing-list.js
@@ -0,0 +1,16 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+//Iterate with for loop example
+console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
+
+//Iterate with while loop example
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
+
