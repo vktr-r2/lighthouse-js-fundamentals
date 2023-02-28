dfcbaa27a55ccdaf295e9f43cfc2d82fb0422e25
Viktor Ristic <vktr.r2@protonmail.com>
2023-02-28 00:22:01 -0500

    Compass - ice-cream assignment

---
+++ ice-cream.js
@@ -0,0 +1,18 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+
+//Add new flavour using push method
+iceCreamFlavours.push('root beer');
+console.log(iceCreamFlavours);
+
+//Log the last first flavour in the array
+console.log(iceCreamFlavours[0]);
+
+//Log the last flavour in the array
+console.log(iceCreamFlavours[5]);
+
+//Log the total number of flavours using .length property
+console.log(iceCreamFlavours.length);
